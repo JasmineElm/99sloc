a729e11c0d1e94a7cd2088b4c8a9271b48c2bbb5
JasmineElm <kj6fi170m6e@riseup.net>
2021-08-07 19:48:41 +0100

    library functions

--- library.sh
+++ library.sh
@@ -24,6 +24,11 @@ _tidy() {
   rm -rf .tmp
 }
 
+_rand_int() {
+  start=$1; step=$2; end=$3
+  seq "$start" "$step" "$end" | shuf -n1
+}
+
 _setup() {
   # each script has its' own $_binaries array
   _tidy
@@ -55,4 +60,18 @@ _format_check(){
   [[ $(_is_valid_format "$ext") = "true" ]] || filename="${filename%.$ext}.$_default_format"
  echo "$filename"
 } 
+
+_rand_float(){
+  printf '%.2f\n' "$(printf '0x0.%04xp1' $RANDOM)"
+}
+
+_rand_string(){
+    local DEFAULT_LENGTH=12;
+    local LENGTH=${1:-$DEFAULT_LENGTH};
+    (tr -dc "[:alnum:]" < /dev/urandom || true) | head -c "$LENGTH" 
+}
+
+_dec_to_hex() {
+  printf '%02x\n' "$1"
+}
 
