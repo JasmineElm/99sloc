2effbd1e64f84e618b0c67107e1166f65c5dd42d
James Lemin <kj6fi170m6e@riseup.net>
2021-09-26 19:56:06 +0100

    abstracted generic functions to library.sh

--- library.sh
+++ library.sh
@@ -7,13 +7,10 @@ IFS=$' \n\t'
 # Wrapper script containing generic functions used in other scripts.
 # Source functions rather than run this script direct
 
-###  VARIABLES    ##########################################
-
 ##!! VARIABLES SET HERE EFFECT _ALL SCRIPTS_
-
-DEFAULT_FORMAT='wav'
+export SOX_OPTS="-V2 -G --magic --multi-threaded --temp .tmp/"
 BINARIES=()
-###  FUNCTIONS    ##########################################
+SOX_FORMAT_ERROR=" doesn't appear to be something sox can handle. exiting"
 
 ###################################################### SETUP
 
@@ -34,7 +31,106 @@ lib::setup() {
   mkdir -p .tmp
 }
 
+lib::print_and_exit() {
+  echo -e "${1:-$HELP_TEXT}"
+  exit
+}
 ######################################################## SOX
+util::tmp_file() {
+  # don't write to the file you're reading from, use a tmp file...
+  local out_file=${1-}
+  local file_ext
+  file_ext="${out_file##*.}"
+  echo "$(date +%s).$file_ext"
+
+}
+
+sox::pan() {
+  local in_file=${1-}
+  local r_vol=${2:-0.5} # 0 = full left-pan, 1 = full right-pan
+  local l_vol
+  l_vol=$(echo "1 - $r_vol" | bc)
+  local out_file
+  out_file=$(util::tmp_file "$in_file")
+  # assumes stereo file
+  [[ $(soxi -c "$in_file") == 2 ]] || lib::print_and_exit "wrong number of channels, exiting"
+  echo sox "$in_file" "$out_file" remix 1v"$l_vol" 2v"$r_vol"
+  sox "$in_file" "$out_file" remix 1v"$l_vol" 2v"$r_vol"
+  mv "$out_file" "$in_file"
+}
+
+sox::pitch() {
+  #pitch is in cents, range here is 0..1 (half-speed to double speed)
+  local in_file=${1-}
+  in_file=$(lib::format_check "$in_file")
+  local pitch=${2:-0.5}
+  local pitch_in_cents
+  pitch_in_cents=$(util::one_range_to_another 0 1 -1200 1200 "$pitch")
+  local out_file=${3-}
+  [[ -z $out_file ]] && out_file=$(util::tmp_file "$in_file") && tmp=true
+  sox "$in_file" "$out_file" pitch "$pitch_in_cents"
+  [[ "$tmp" == true ]] && mv "$out_file" "$in_file"
+}
+
+sox::stretch() {
+  #pitch is in cents, range here is 0..1 (half-speed to double speed)
+  local in_file=${1-}
+  in_file=$(lib::format_check "$in_file")
+  local stretch=${2:-0.5}
+  local stretch_ratio
+  stretch_ratio=$(util::one_range_to_another 0 1 0.5 2.0 "$stretch" false)
+  local out_file=${3-}
+  [[ -z $out_file ]] && out_file=$(util::tmp_file "$in_file") && tmp=true
+  sox "$in_file" "$out_file" stretch "$stretch_ratio"
+  [[ "$tmp" == true ]] && mv "$out_file" "$in_file"
+}
+
+sox::generic(){
+  ## generic wrapper for single parameter effects
+  # usage: $0 $in_file $effect $strength [$out_file]
+  local in_file=${1-}
+  in_file=$(lib::format_check "$in_file")
+  local effect=${2-}
+  local strength=${3-0.5}
+  local out_file=${4-}
+  [[ -z $out_file ]] && out_file=$(util::tmp_file "$in_file") && tmp=true
+  ## different effects have different ranges. 
+  local min
+  local max
+  case $effect in
+    pitch)
+      min=-1200
+      max=1200
+      ;;
+    *)
+      min=0.5
+      max=2.0
+      ;;
+  esac
+  strength=$(util::one_range_to_another 0 1 "$min" "$max" "$strength" false)
+  sox "$in_file" "$out_file" "$effect" "$strength"
+  [[ "$tmp" == true ]] && mv "$out_file" "$in_file"
+}
+
+sox::speed() {
+  local in_file=${1-}
+  in_file=$(lib::format_check "$in_file")
+  local speed=${2:-1.0} #range 0..4.0
+  speed=$(util::one_range_to_another 0 1 0 4.0 "$speed" false)
+  local out_file=${3:-$in_file}
+  [[ -z $out_file ]] && out_file=$(util::tmp_file "$in_file") && tmp=true
+  sox "$in_file" "$out_file" speed "$speed"
+  [[ "$tmp" == true ]] && mv "$out_file" "$in_file"
+}
+
+sox::reverse() {
+  local in_file=${1-}
+  in_file=$(lib::format_check "$in_file")
+  local out_file=${3:-$in_file}
+  [[ -z $out_file ]] && out_file=$(util::tmp_file "$in_file") && tmp=true
+  sox "$in_file" "$out_file" reverse
+  [[ "$tmp" == true ]] && mv "$out_file" "$in_file"
+}
 
 lib::is_valid_format() {
   # is our file format one that sox can handle?
@@ -54,12 +150,14 @@ lib::is_valid_format() {
 
 lib::format_check() {
   #filename.ext - is it valid?
-  # looks for anything after the '.'
   local filename
   filename=${1-}
   ext=${filename#*.}
-  [[ $(lib::is_valid_format "$ext") = "true" ]] || filename="${filename%.$ext}.$DEFAULT_FORMAT"
-  echo "$filename"
+  if [[ $(lib::is_valid_format "$ext") = "true" ]]; then
+    echo "$filename"
+  else
+    lib::print_and_exit "$SOX_FORMAT_ERROR"
+  fi
 }
 
 ##################################################### RANDOM
@@ -108,11 +206,44 @@ lib::pseudo_word() {
   local rand_string
   rand_string=$(lib::rand_string 1 1 $rand_str_len)
   echo "$prefix$rand_string$suffix"
-
 }
 
+lib::random_word() {
+  #random word for youtube search
+  shuf -n 1 /usr/share/dict/words
+}
 #################################################### HELPERS
 
-lib::dec_to_hex() {
+util::dec_to_hex() {
   printf '%02x\n' "$1"
+}
+
+util::round_float() {
+  local in_float=${1-}
+  local precision=${2:-0}
+  echo "$in_float" | LC_ALL=C xargs /usr/bin/printf "%0.*f\\n" "$precision"
+}
+
+util::one_range_to_another() {
+  # $0 old_min old_max new_min new_max in_val
+  local from_min=${1:-0}
+  local from_max=${2:-1}
+  local from_range
+  from_range=$(echo "$from_max - $from_min" | bc)
+
+  local to_min=${3:-0}
+  local to_max=${4:-255}
+  local to_range
+  to_range=$(echo "$to_max - $to_min" | bc)
+
+  local in_val=${5-}
+  local int=${6:-"true"}
+  local out_val
+  out_val=$(echo "$in_val * ($to_range / $from_range) + $to_min" | bc)
+  if [[ "$int" == "true" ]]; then
+    out_val=$(util::round_float "$out_val")
+  else
+    out_val=$(util::round_float "$out_val" 2)
+  fi
+  echo "$out_val"
 }
